ad84a4b8730efe6428c4dbca65d7573e01956cf6
Jennifer Liu <jenniferliu716@gmail.com>
2016-06-01 10:46:06 -0700

    add activeUser and tempScore to localStorage for return player purpose

--- js/login.js
+++ js/login.js
@@ -70,24 +70,21 @@ function handlePlayerLogin(event) {
   event.preventDefault(); //prevents page reload
   var hName = event.target.username.value;
   var hScore = event.target.userscore.value;//for testing purpose
+  var activeUser = true;
+  var tempScore = 0;
   for(var i = 0; i < allPlayer.length; i++) {
     if (hName === allPlayer[i].name){
-      event.target.username.value = null;
-      event.target.userscore.value = null;
-      return document.getElementById('duplicate').innerHTML = 'The name already exists! Please re-enter new name';
+      tempScore = allPlayer[i].score;
+      // event.target.username.value = null;
+      // event.target.userscore.value = null;
+      // return document.getElementById('duplicate').innerHTML = 'The name already exists! Please re-enter new name';
     }
   }
-  // document.getElementById('duplicate').innerHTML = '';
-  // var newPlayer = new Player(hName, hScore);
-  // console.log(allPlayer);
-  // outputTable();
+  localStorage.setItem('activeUser', JSON.stringify(activeUser));
+  localStorage.setItem('tempScore', JSON.stringify(tempScore));
   localStorage.setItem('name', JSON.stringify(hName));
   localStorage.setItem('score', JSON.stringify(hScore));// for testing purpose
   localStorage.setItem('allData', JSON.stringify(rankPlayer));
-
-  // event.target.username.value = null;
-  // event.target.userscore.value = null;
-  // console.log('You just cleared all the fields!');
   location.assign('game.html');
 }
 
