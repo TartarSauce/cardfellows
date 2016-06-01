820b33d29c7ba079a9028d97d9f48c7e447c6c29
COREY MALEK <COREYMALEK1@GMAIL.COM>
2016-06-01 15:16:32 -0700

    fixed index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var pressStart = document.getElementById('inner-play');
 pressStart.addEventListener('click', checkActiveUser);
 
-
 function checkActiveUser() {
   var activeUser = JSON.parse(localStorage.getItem('activeUser'));
   if (activeUser){
